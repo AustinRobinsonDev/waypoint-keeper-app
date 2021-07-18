23ca75f441d741c1ec813c3d317ef88b2cce56ad
AustinRobinsonDev <austin.worktrux@gmail.com>
2021-07-18 13:41:46 -0500

    fixed where map was being run b4 data was fetched

--- client/src/components/waypoints/MapApp.js
+++ client/src/components/waypoints/MapApp.js
@@ -4,19 +4,15 @@ import ReactMapGL, { Marker } from 'react-map-gl';
 import WaypointContext from '../../context/waypoint/waypointContext';
 
 const MapApp = () => {
- 
+  useEffect(() => {
+    getWaypoints();
+    //console.log()
+    //eslint-disable-next-line
+},[])
   const waypointContext = useContext(WaypointContext);
-  const { waypoints, getWaypoints } = waypointContext;
+  const { waypoints, getWaypoints, loading } = waypointContext;
   //const [mappedWaypoints, setMappedWaypoints] = useState([waypoints]);
   // const { _id, name, tag, lat, lon} = waypoint;
-
-  useEffect(() => {
-        getWaypoints();
-        //console.log()
-        //eslint-disable-next-line
-    },[])
-
-
   const [viewport, setViewport] = useState({
     latitude: 45.4311,
     longitude: -75.3535,
@@ -26,15 +22,15 @@ const MapApp = () => {
 });
       return (
     <div>
-        <ReactMapGL {...viewport} {...waypoints} onViewportChange={(viewport) => {
+        <ReactMapGL {...viewport} onViewportChange={(viewport) => {
             setViewport(viewport)
         }} 
         mapboxApiAccessToken="pk.eyJ1IjoiYXVzdGlucm9iaW5zb24yMDI0IiwiYSI6ImNrcjE1em5qNDAxbXkybm4zbWs3b3h3YTEifQ.b3P8rKXgjapDCkXQ_qplPw"
         mapStyle='mapbox://styles/austinrobinson2024/ckr2iles21xj917nzmwcya94g'
         >
-          {waypoints.map((singlePoint) => (<Marker key={singlePoint._id} latitude={singlePoint.position[0]} longitude={singlePoint.position[1]} >
+          {waypoints !== null ? (waypoints.map(singlePoint => (<Marker key={singlePoint._id} latitude={singlePoint.position[0]} longitude={singlePoint.position[1]} >
               <div><p style={{ color: 'white'}}>{singlePoint.name}</p></div>
-            </Marker>))}
+            </Marker>))) : (<p>please enter waypoints</p>)}
         </ReactMapGL>
     </div>
   );
